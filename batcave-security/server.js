a1574a68fc814d8f86e65fdc73624a652cff1143
vithurzen <vithurzen517@gmail.com>
2026-06-12 18:07:57 +0200

    PHASE 2.1 : ROBUSTESSE DU REGISTRE

--- batcave-security/server.js
+++ batcave-security/server.js
@@ -19,6 +19,12 @@ app.listen(PORT, () => {
 app.post('/register', async (req, res) => {
   // Récupère les identifiants saisis par l'utilisateur
   const { username, password } = req.body
+  username.trim()
+
+  if (password.length < 8) {
+    return res.status(400).send('Le mot de passe doit contenir au moins 8 caractères.')
+  }
+
 
   // Hachage du mot de passe avant stockage !
   const hash = await bcrypt.hash(password, 10)
@@ -31,6 +37,9 @@ app.post('/register', async (req, res) => {
     insert.run(username, hash)
     res.status(201).send('Utilisateur créé avec succès !')
   } catch (err) {
-    res.status(400).send("Erreur : l'utilisateur existe déjà.")
+    if (err.code === 'SQLITE_CONSTRAINT_UNIQUE') {
+      return res.status(409).send("Ce nom d'utilisateur est déjà utilisé.")
+    }
+    res.status(500).send('Erreur interne du serveur.')
   }
 })
