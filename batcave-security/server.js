537877a41fe7f18abc0d4de2c02406837f7308c3
vithurzen <vithurzen517@gmail.com>
2026-07-05 11:58:42 +0200

    PHASE 3 : PROTECTION DU BATORDINATEUR

--- batcave-security/server.js
+++ batcave-security/server.js
@@ -43,3 +43,56 @@ app.post('/register', async (req, res) => {
     res.status(500).send('Erreur interne du serveur.')
   }
 })
+
+const checkAuth = async (req, res, next) => {
+  // Récupère l'en-tête pour la vérifier avant d'atteindre les routes protégées
+  const authHeader = req.headers.authorization
+
+  if (!authHeader || !authHeader.startsWith('Basic ')) {
+    // Ajoute l'en-tête pour demander au navigateur d'ouvrir la fenêtre de connexion
+    res.setHeader('WWW-Authenticate', 'Basic realm="Administration"')
+    return res.status(401).send('Authentification requise')
+  }
+  // Décodage du Base64
+  const base64 = authHeader.split(' ')[1]
+  const [username, password] = Buffer.from(base64, 'base64')
+    .toString()
+    .split(':')
+
+  // Vérification en BDD 
+  const user = db
+    .prepare('SELECT * FROM users WHERE username = ?')
+    .get(username)
+  // Comparaison des mots de passe avec bcrypt
+  if (user && (await bcrypt.compare(password, user.password_hash))) {
+    req.user = user // On conserve l'utilisateur dans la requête, si besoin
+    next()
+  } else {
+    return res.status(401).send('Identifiants invalides')
+  }
+}
+
+app.get('/bat-computer', checkAuth, (req, res) => {
+  res.sendFile(__dirname + '/private/bat-computer.html')
+})
+
+app.get('/api/secrets', checkAuth, (req, res) => {
+  const gadgets = [
+    {
+      "name": "Batarang",
+      "desc": "Arme de jet",
+      "icon": "fa-shuriken"
+    },
+    {
+      "name": "Grapnel Gun",
+      "desc": "Pistolet-grappin pour se déplacer rapidement",
+      "icon": "fa-anchor"
+    },
+    {
+      "name": "Smoke Bomb",
+      "desc": "Bombe fumigène pour disparaître discrètement",
+      "icon": "fa-cloud"
+    }
+  ]
+  res.json(gadgets)
+})
